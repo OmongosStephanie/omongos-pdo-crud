c141db8702fb6bc7eae3d2af25a9823860312faf
tepani <stephanieomongos05@gmail.com>
2024-05-20 15:43:13 +0800

    added changes

--- db/luna.sql
+++ db/luna.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 06, 2024 at 10:18 AM
+-- Generation Time: May 20, 2024 at 09:42 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.0.28
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `charmaine`
+-- Database: `luna`
 --
 
 -- --------------------------------------------------------
